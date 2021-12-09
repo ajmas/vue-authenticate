ee11413bc9c44b2a4b6ba53e4791c2e65e86913d
Andre-John Mas <andrejohn.mas@gmail.com>
2021-12-08 23:58:02 -0500

    vue3 changes

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
 import './utils.js';
 import VueAuthenticate from './authenticate.js';
 
+const vueAuthInstance;
 const VueAuthenticatePlugin = {
   install(app, options) {
     if (!options) {
       options = {};
     }
 
-    app.config.globalProperties.$auth = {
-      get() {
-        if (!vueAuthInstance) {
-          let axios;
-          // if an axios instance is passed then use that,
-          // then try to see if there is a instance referenced via
-          // the $http instance property, otherwie fail
-          if (options.axios) {
-            axios = options.axios;
-          } else if (this.$http) {
-            axios = this.$http;
-          } else {
-            throw new Error('Request handler instance not found');
-          }
-
-          vueAuthInstance = new VueAuthenticate(axios, options);
-        }
-        return vueAuthInstance;
+    if (!vueAuthInstance) {
+      let axios;
+      // if an axios instance is passed then use that,
+      // then try to see if there is a instance referenced via
+      // the $http instance property, otherwie fail
+      if (options.axios) {
+        axios = options.axios;
+      } else if (this.$http) {
+        axios = this.$http;
+      } else {
+        throw new Error('Request handler instance not found');
       }
-    };
-  },
+
+      vueAuthInstance = new VueAuthenticate(axios, options);
+      app.config.globalProperties.$auth = vueAuthInstance;
+    }
+  }
 };
 
 export default VueAuthenticatePlugin;
