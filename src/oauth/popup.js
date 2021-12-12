9c9e044bbc5c82f053fbe382b88fc24ecfb3f31a
Andre-John Mas <andrejohn.mas@gmail.com>
2021-12-11 19:09:08 -0500

    convert project to async/await
    Note had to changethe version of rollup used, since the
    bundled one broke with async/await. In doing so, dropped
    support for commonjs.

--- src/oauth/popup.js
+++ src/oauth/popup.js
@@ -1,4 +1,3 @@
-import Promise from '../promise.js';
 import { $document, $window } from '../globals.js';
 import {
   objectExtend,
@@ -22,76 +21,70 @@ export default class OAuthPopup {
     this.popupOptions = popupOptions;
   }
 
-  open(redirectUri, skipPooling) {
+  async open(redirectUri, skipPooling) {
     try {
       this.popup = $window.open(this.url, this.name, this._stringifyOptions());
       if (this.popup && this.popup.focus) {
         this.popup.focus();
       }
 
-      if (skipPooling) {
-        return Promise.resolve();
-      } else {
-        return this.pooling(redirectUri);
+      if (!skipPooling) {
+        return this.pooling(redirectUri)
       }
-    } catch (e) {
-      return Promise.reject(new Error('OAuth popup error occurred'));
+    } catch (error) {
+      throw new Error('OAuth popup error occurred');
     }
   }
 
-  pooling(redirectUri) {
-    return new Promise((resolve, reject) => {
-      const redirectUriParser = $document.createElement('a');
-      redirectUriParser.href = redirectUri;
-      const redirectUriPath = getFullUrlPath(redirectUriParser);
+  async pooling(redirectUri) {
+    const redirectUriParser = $document.createElement('a');
+    redirectUriParser.href = redirectUri;
+    const redirectUriPath = getFullUrlPath(redirectUriParser);
 
-      let poolingInterval = setInterval(() => {
-        if (
-          !this.popup ||
-          this.popup.closed ||
-          this.popup.closed === undefined
-        ) {
-          clearInterval(poolingInterval);
-          poolingInterval = null;
-          reject(new Error('Auth popup window closed'));
-        }
+    let poolingInterval = setInterval(() => {
+      if (
+        !this.popup ||
+        this.popup.closed ||
+        this.popup.closed === undefined
+      ) {
+        clearInterval(poolingInterval);
+        poolingInterval = null;
+        throw new Error('Auth popup window closed');
+      }
 
-        try {
-          const popupWindowPath = getFullUrlPath(this.popup.location);
+      try {
+        const popupWindowPath = getFullUrlPath(this.popup.location);
 
-          if (popupWindowPath === redirectUriPath) {
-            if (this.popup.location.search || this.popup.location.hash) {
-              const query = parseQueryString(
-                this.popup.location.search.substring(1).replace(/\/$/, '')
-              );
-              const hash = parseQueryString(
-                this.popup.location.hash.substring(1).replace(/[\/$]/, '')
-              );
-              let params = objectExtend({}, query);
-              params = objectExtend(params, hash);
+        if (popupWindowPath === redirectUriPath) {
+          if (this.popup.location.search || this.popup.location.hash) {
+            const query = parseQueryString(
+              this.popup.location.search.substring(1).replace(/\/$/, '')
+            );
+            const hash = parseQueryString(
+              this.popup.location.hash.substring(1).replace(/[\/$]/, '')
+            );
+            let params = objectExtend({}, query);
+            params = objectExtend(params, hash);
 
-              if (params.error) {
-                reject(new Error(params.error));
-              } else {
-                resolve(params);
-              }
+            if (params.error) {
+              throw new Error(params.error);
             } else {
-              reject(
-                new Error(
-                  'OAuth redirect has occurred but no query or hash parameters were found.'
-                )
-              );
+              return params;
             }
-
-            clearInterval(poolingInterval);
-            poolingInterval = null;
-            this.popup.close();
+          } else {
+            throw new Error(
+                'OAuth redirect has occurred but no query or hash parameters were found.'
+              );
           }
-        } catch (e) {
-          // Ignore DOMException: Blocked a frame with origin from accessing a cross-origin frame.
         }
-      }, 250);
-    });
+      } catch (error) {
+        // Ignore DOMException: Blocked a frame with origin from accessing a cross-origin frame.
+      } finally {
+        clearInterval(poolingInterval);
+        poolingInterval = null;
+        this.popup.close();
+      }
+    }, 250);
   }
 
   _stringifyOptions() {
