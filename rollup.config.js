c4f030e2978c8bb0c6313538c8227ca20851d403
Andre-John Mas <andrejohn.mas@gmail.com>
2021-12-11 21:07:14 -0500

    uglify instead of terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
-// import { terser } from "rollup-plugin-terser";
+import { uglify } from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
 export default {
     input: 'src/index.js',
     plugins: [
-        // terser()
+        uglify({
+            sourcemap: true
+        })
     ],
     output: [
         {
